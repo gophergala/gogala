5dcee9da085ed33b7aea8505eceef937bb78fc5b
Julien Castelain <jcastelain@gmail.com>
2015-01-24 02:45:25 +0100

    edit(main): add gzip/cache headers

--- main.go
+++ main.go
@@ -1,12 +1,33 @@
 package main
 
-import "net/http"
+import (
+	"flag"
+	"log"
+	"net/http"
+
+	"github.com/julien/gogala/util"
+)
+
+var (
+	listenAddr = flag.String("addr", "8000", "Listen address")
+	debug      util.Debug
+	verbose    bool
+)
+
+func init() {
+	flag.BoolVar(&verbose, "verbose", true, "Debug mode")
+}
 
 func main() {
+	flag.Parse()
+	debug = util.Debug(verbose)
 
 	http.Handle("/", indexHandler())
-	http.Handle("/static/", staticHandler())
-	http.ListenAndServe(":8080", nil)
+	http.Handle("/static/", util.GZipHandler(util.CacheHandler(30, staticHandler())))
+
+	debug.Printf("Listening on: %s\n", *listenAddr)
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 func indexHandler() http.Handler {
